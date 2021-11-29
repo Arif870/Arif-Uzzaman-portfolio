b060c4f4a0703ba0793e9acf8daf739e7b4d2ecd
Arif870 <arifuzzaman959@gmail.com>
2021-11-29 22:20:16 +0600

    menu redesigned by boorstrap

--- src/Components/Menu/Menu.js
+++ src/Components/Menu/Menu.js
@@ -1,30 +1,55 @@
+import { Button } from "@mui/material";
 import * as React from "react";
-import AppBar from "@mui/material/AppBar";
-import Box from "@mui/material/Box";
-import Toolbar from "@mui/material/Toolbar";
-import Typography from "@mui/material/Typography";
-import Button from "@mui/material/Button";
 import logo from "../../media/logo.png";
 import "./Menu.css";
 
 export default function Menu() {
   return (
-    <Box sx={{ flexGrow: 1 }}>
-      <AppBar position="static">
-        <Toolbar>
-          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-            <img style={{ width: "100px" }} src={logo} alt="" />
-          </Typography>
-          <Button style={{ fontFamily: "poppins" }} variant="contained">
-            <a
-              style={{ textDecoration: "none", color: "white" }}
-              href="mailto: arifuzzaman959@gmail.com"
-            >
-              Hire me
-            </a>
-          </Button>
-        </Toolbar>
-      </AppBar>
-    </Box>
+    <nav className="navbar navbar-expand-lg mt-3">
+      <img style={{ width: "80px" }} src={logo} alt="logo" />
+
+      <button
+        className="navbar-toggler"
+        type="button"
+        data-toggle="collapse"
+        data-target="#navbarTogglerDemo02"
+        aria-controls="navbarTogglerDemo02"
+        aria-expanded="false"
+        aria-label="Toggle navigation"
+      >
+        <span className="navbar-toggler-icon"></span>
+      </button>
+
+      <Button
+        className="ms-auto"
+        style={{ fontFamily: "poppins" }}
+        variant="contained"
+      >
+        <a
+          style={{ textDecoration: "none", color: "white" }}
+          href="mailto: arifuzzaman959@gmail.com"
+        >
+          Hire me
+        </a>
+      </Button>
+    </nav>
+
+    // <Box sx={{ flexGrow: 1 }}>
+    //   <AppBar position="static">
+    //     <Toolbar>
+    //       <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+    //         <img style={{ width: "100px" }} src={logo} alt="" />
+    //       </Typography>
+    //       <Button style={{ fontFamily: "poppins" }} variant="contained">
+    //         <a
+    //           style={{ textDecoration: "none", color: "white" }}
+    //           href="mailto: arifuzzaman959@gmail.com"
+    //         >
+    //           Hire me
+    //         </a>
+    //       </Button>
+    //     </Toolbar>
+    //   </AppBar>
+    // </Box>
   );
 }
