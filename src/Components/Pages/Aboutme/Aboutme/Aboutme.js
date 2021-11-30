7cf138545c715dfe36371ba53bc0413919743c9d
Arif870 <arifuzzaman959@gmail.com>
2021-11-30 17:54:57 +0600

    about us page added

--- src/Components/Pages/Aboutme/Aboutme/Aboutme.js
+++ src/Components/Pages/Aboutme/Aboutme/Aboutme.js
@@ -1,12 +1,14 @@
 import React from "react";
 import Menu from "../../../Shared/Menu/Menu";
 import aboutimg from "../../../../media/aboutme.png";
+import Footer from "../../../Shared/Footer/Footer";
+import LabTabs from "../LabTabs/LabTabs";
 
 export default function Aboutme() {
   return (
     <div className="container">
       <Menu />
-      <img className="w-100" src={aboutimg} alt="" />
+      <img className="w-75 mx-auto d-block" src={aboutimg} alt="" />
       <p className="text-center mt-4">
         <span className="text-info fw-bold fs-4">
           I am Arif-uz-zaman. Full stack web developer ( MERN )
@@ -17,6 +19,8 @@ export default function Aboutme() {
         university <br />
         Department of Computer Science and Engineering .
       </p>
+      <LabTabs />
+      <Footer />
     </div>
   );
 }
